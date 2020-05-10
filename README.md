bfe19a8c1222eb25af7c4ef11c7caa77ffcd1f73
SChapin97 <samuelchapin97@gmail.com>
2020-05-10 13:01:41 -0500

    Updated main.py and README, added praw.ini

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Reddit-Scraper
+A python-based script that will display a user's top posts from each of their multireddits.
+
+# Requirements:
+Requirements:
+   python3
+   pip
+   praw (pip install praw)
+   a text editor
+
+# Usage:
+Usage:
+Fill in your details in praw.ini (client_id, client_secret for a reddit script; username and password for a reddit account)
+Run main.py
--- main.py
+++ main.py
@@ -4,14 +4,26 @@ import re
 
 reddit = praw.Reddit("bot1", user_agent="Post Scraper")
 
-bapcs = reddit.subreddit("buildapcsales")
+def multi_reddit():
+    u_user = reddit.user.me()
+    u_mreddits = u_user.multireddits()
 
-totalPosts=[]
+    for mreddit in u_mreddits:
+    	print(mreddit.name, mreddit.visibility)
+        for subreddit in mreddit.subreddits:
+            print("   " + subreddit.display_name)
+            top_posts(subreddit)
 
-for post in bapcs.hot():
-    # print("totalPosts length:", len(totalPosts))
-    if len(totalPosts) == 10:
-        break;
+
+def bapcs_posts():
+    bapcs = reddit.subreddit("buildapcsales")
+
+    totalPosts = []
+
+    for post in bapcs.hot():
+        # print("totalPosts length:", len(totalPosts))
+        if len(totalPosts) == 10:
+            break
     # print(totalPosts)
 
     # print(post.title, "; ", post.score, post.created_utc)
@@ -24,3 +36,10 @@ for post in bapcs.hot():
             print("Type:", m.group("type"), "; Item:", m.group("item"), "; Price:", m.group("price"))
             totalPosts.append(post)
             print("score per minute:", minutesSince // post.score, "; Title:", post.title)
+
+
+def top_posts(subreddit):
+    for post in subreddit.top("day", limit=3):
+        print("      Points:", post.score, "Title:", post.title)
+
+multi_reddit()
