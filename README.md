71902752f245ed691e8e4b7975308291d9c0fdd3
SChapin97 <samuelchapin97@gmail.com>
2020-05-10 15:16:09 -0500

    Completed functionality for getting top 5 posts for each subreddit per multireddit (including score, comments, and links to both the post and any link included in the post)

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 A python-based script that will display a user's top posts from each of their multireddits.
 
 # Requirements:
-   python3;
-   pip;
-   praw (pip install praw);
-   a text editor;
+   python3
+
+   pip
+
+   praw (pip install praw)
+
+   a text editor
+
 
 # Usage:
-Fill in your details in praw.ini (client_id, client_secret for a reddit script; username and password for a reddit account);
+Fill in your details in praw.ini (client_id, client_secret for a reddit script; username and password for a reddit account)
 
 Run main.py
--- main.py
+++ main.py
@@ -9,10 +9,20 @@ def multi_reddit():
     u_mreddits = u_user.multireddits()
 
     for mreddit in u_mreddits:
-    	print(mreddit.name, mreddit.visibility)
+        print(mreddit.name)
         for subreddit in mreddit.subreddits:
-            print("   " + subreddit.display_name)
+            print(" " + subreddit.display_name)
             top_posts(subreddit)
+
+
+def top_posts(subreddit):
+    for post in subreddit.top("day", limit=5):
+        print("   ", post.title)
+        print("       Score:", str(post.score) + " (" + str(int(post.upvote_ratio * 100)) + "%);",
+              "Comments:", post.num_comments)
+        print("       Post:", "https://www.reddit.com" + post.permalink)
+        if post.url != "https://www.reddit.com" + post.permalink:
+            print("       Link:", post.url)
 
 
 def bapcs_posts():
@@ -36,10 +46,5 @@ def bapcs_posts():
             print("Type:", m.group("type"), "; Item:", m.group("item"), "; Price:", m.group("price"))
             totalPosts.append(post)
             print("score per minute:", minutesSince // post.score, "; Title:", post.title)
-
-
-def top_posts(subreddit):
-    for post in subreddit.top("day", limit=3):
-        print("      Points:", post.score, "Title:", post.title)
 
 multi_reddit()
