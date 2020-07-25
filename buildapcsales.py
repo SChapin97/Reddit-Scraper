7af632f381db2a28e27082227634c96e391fb6e1
Sam Chapin <samuelchapin97@gmail.com>
2020-07-25 17:52:31 +0000

    Fixed some logic pathways to make the script run faster.

--- buildapcsales.py
+++ buildapcsales.py
@@ -1,10 +1,10 @@
 import praw
 import time
 import re
-import os.path
+import os
 
 reddit = praw.Reddit("bot1", user_agent="Post Scraper")
-searchItems = ["CPU"]
+searchItems = ["print", "lamp", "ikea"]
 post_redundancy_file_path = "previous_posts"
 
 def bapcs_posts():
@@ -16,38 +16,36 @@ def bapcs_posts():
         f.close()
 
     for post in bapcs.new(): #, limit=30):
-        m = re.search("(?P<type>\\[[^\\[\\]]+\\])\\s*(?P<item>[^$]+)(?P<price>\\$.+)", post.title)
-        if m:
-            printPost(post, m)
-
-
-def printPost(post, m):
-    #Looking to see if the ID has already been found by the script
-    redundant_file = open(post_redundancy_file_path, "r")
-    found = False
-
-    for line in redundant_file.readlines():
-        if post.id in line:
-            found = True
-
-    #If the post ID has not been found, then we want to print the post information
-    if not found:
-        redundant_file.close()
-        redundant_file = open(post_redundancy_file_path, "a")
-
-        #Only want to print out items that are specified in the searchItems array.
+        printable = False
         for item in searchItems:
             if item.upper() in post.title.upper():
-                minutesSince = (time.time() - post.created_utc) // 60
+                printable = True
+                break
 
+        if not printable:
+            continue
+
+        m = re.search("(?P<type>\\[[^\\[\\]]+\\])\\s*(?P<item>[^$]+)(?P<price>\\$.+)", post.title)
+        if m:
+            #Looking to see if the ID has already been found by the script
+            redundant_file = open(post_redundancy_file_path, "r")
+            found = False
+
+            for line in redundant_file.readlines():
+                if post.id in line:
+                    found = True
+
+            #If the post ID has not been found, then we want to print the post information
+            if not found:
+                redundant_file = open(post_redundancy_file_path, "a")
                 #Adding to the redundant post file
                 redundant_file.write(post.id + "\r\n")
+                redundant_file.close()
+                printPost(post)
 
-                print(post.title, "; Upvotes:", post.score) #, post.created_utc)
-                print("Reddit link: https://www.reddit.com/r/buildapcsales/" + post.id)
-                print("Post link:", post.url)
-#                print("Type:", m.group("type"), "; Item:", m.group("item"), "; Price:", m.group("price"))
-
-    redundant_file.close()
+def printPost(post):
+    print(post.title, "; Upvotes:", post.score) #, post.created_utc)
+    print("Reddit link: http://redd.it/" + post.id)
+    print("Post link:", post.url)
 
 bapcs_posts()
