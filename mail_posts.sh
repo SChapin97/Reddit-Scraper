0870d0ef60ecc0deeaf2b85bc8cb4968bd1cf079
Sam Chapin <samuelchapin97@gmail.com>
2020-08-08 17:49:05 +0000

    Fixing html output to mail.
    Still needs a lot of CSS work to look good, however.

--- mail_posts.sh
+++ mail_posts.sh
@@ -4,8 +4,7 @@
 python3 buildapcsales.py > mailfile
 
 if [[ -f "mailfile" && -s "mailfile" ]]; then
-   #mail -r "sender@example.com" -s "buildapcsales alert" "recipient@example.com" < mailfile
-   cat template.html mailfile | mail -r "sender@example.com" -s "buildapcsales alert" "recipient@example.com" < template.html mailfile
+   mail -a "Content-type: text/html" -r "sender@example.com" -s "buildapcsales alert" "recipient@example.com" < <(cat template.html mailfile)
 fi
 
 rm mailfile
@@ -15,8 +14,7 @@ rm mailfile
 python3 laptopdeals.py > mailfile
 
 if [[ -f "mailfile" && -s "mailfile" ]]; then
-   #mail -r "sender@example.com" -s "laptopdeals alert" "recipient@example.com" < mailfile
-   cat template.html mailfile | mail -r "sender@example.com" -s "laptopdeals alert" "recipient@example.com" < template.html mailfile
+   mail -a "Content-type: text/html" -r "sender@example.com.com" -s "laptopdeals alert" "recipient@example.com" < <(cat template.html mailfile)
 fi
 
 rm mailfile
