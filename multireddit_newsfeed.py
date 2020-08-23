9cf50d175f6a3f4fc63c7b762298faf0d5823e04
Sam Chapin <schapin@samuelchapin.com>
2020-08-22 23:58:49 -0500

    Fixing image size issues and html escaping.

--- multireddit_newsfeed.py
+++ multireddit_newsfeed.py
@@ -1,6 +1,8 @@
 import praw
+import html
 
 reddit = praw.Reddit("bot1", user_agent="Post Scraper")
+IMAGE_FORMATS = ["jpeg", "jpg", "png", "gif"]
 
 def multi_reddit():
     u_user = reddit.user.me()
@@ -16,15 +18,29 @@ def multi_reddit():
 
 def top_posts(subreddit):
     for post in subreddit.top("day", limit=5):
+        #If a url starts with /r/, it is a x-post and needs to have reddit.com prepended to it.
+        url = post.url
+        if url.startswith('/r/'):
+            url = 'https://reddit.com' + url
+
         print("\t\t<div>")
-        print("\t\t\t<a href=\"http://redd.it/" + post.id + "\">" + post.title + "</a>")
-        #TODO: Fix type conversion issues
-        #print("\t\t\t<p>Score:", str(post.score) + " (" + str(int(post.upvote_ratio * 100)) + "%);", "Comments:", post.num_comments +"</p>")
+        print("\t\t\t<a href=\"http://redd.it/" + post.id + "\" class=\"post\">" + html.escape(post.title) + "</a>")
         if "http" in post.thumbnail:
-            print("\t\t\t<img src=\"" + post.thumbnail + "\" alt = \"Thumbnail image from the post\">")
-        if (not "redd.it" in post.url) and (not post.id in post.url) :
-            print("\t\t\t<a href=\"" + post.url + "\">Outgoing link</a>")
+            if is_image(url):
+                print("\t\t\t<a href=\"" + url + "\" class=\"image\"><img src=\"" + url + "\" alt=\"Full sized image from the post\" class=\"image\"></a>")
+            else:
+                print("\t\t\t<a href=\"" + url + "\" class=\"image\"><img src=\"" + post.thumbnail + "\" alt=\"Thumbnail image from the post\" class=\"image\"></a>")
+
+        elif (not "redd.it" or "reddit" in url) and (not post.id in url) :
+            print("\t\t\t<a href=\"" + url + "\" class=\"link\">Outgoing link</a>")
         print("\t\t</div>")
 
+
+def is_image(url):
+    for format in IMAGE_FORMATS:
+         if url.endswith(format):
+             return True
+    else:
+        return False
 
 multi_reddit()
