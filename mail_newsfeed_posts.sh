3412ecdb79831cbfefb8a62746f59753cb23451e
Sam Chapin <samuelchapin97@gmail.com>
2020-10-17 14:28:47 -0500

    Fixing accidental syntax issue

--- mail_newsfeed_posts.sh
+++ mail_newsfeed_posts.sh
@@ -6,20 +6,20 @@ date=`date "+%A %b %d"`
 #News multireddit
 cat template.html > output.html
 python3 multireddit_newsfeed.py news >> output.html
-mail -a ail -a "Content-type: text/html" -r "sender@example.com" -s "Daily News Multireddit Feed - $date" "recipient@example.com" < output.html
+mail -a "Content-type: text/html" -r "sender@example.com" -s "Daily News Multireddit Feed - $date" "recipient@example.com" < output.html
 
 rm output.html
 
 #Dev multireddit
 cat template.html > output.html
 python3 multireddit_newsfeed.py dev >> output.html
-mail -a ail -a "Content-type: text/html" -r "sender@example.com" -s "Daily Dev Multireddit Feed - $date" "recipient@example.com" < output.html
+mail -a "Content-type: text/html" -r "sender@example.com" -s "Daily Dev Multireddit Feed - $date" "recipient@example.com" < output.html
 
 rm output.html
 
 #Memes multireddit
 cat template.html > output.html
 python3 multireddit_newsfeed.py memes >> output.html
-mail -a ail -a "Content-type: text/html" -r "sender@example.com" -s "Daily Memes Multireddit Feed - $date" "recipient@example.com" < output.html
+mail -a "Content-type: text/html" -r "sender@example.com" -s "Daily Memes Multireddit Feed - $date" "recipient@example.com" < output.html
 
 rm output.html
